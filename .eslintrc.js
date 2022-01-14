934c3962f7fd0ef9b02b34e13fb00dafd7fc0a79
p4avinash <p4avinashmaurya@gmail.com>
2022-01-14 11:25:01 +0530

    husky test

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["google"],
+  parserOptions: {
+    ecmaVersion: 13,
+  },
+  rules: {},
+};
---
+++ app.js
@@ -0,0 +1,3 @@
+const userName = "Avinash";
+
+console.log(userName);
